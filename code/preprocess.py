7270a1429d6862c70c9f7eac9100fb640bbad91e
shifei.chen <chenshifei.j@gmail.com>
2019-12-01 21:48:31 +0100

    Add translation

--- code/preprocess.py
+++ code/preprocess.py
@@ -1,11 +1,14 @@
 import csv
+import demoji
 import pandas as pd
 from opencc import OpenCC
 import preprocessor as p
 import regex
 
 CHN_REGEX = r'[^\p{BasicLatin}]+[[:punct:]]?'
+MEANINGLESS_REGEX = r'[\W\d\s\_]+$'
 cc = OpenCC('s2t')
+demoji.download_codes()
 
 def clean_text(raw_text):
     detweeted_text = p.clean(raw_text)
@@ -13,25 +16,48 @@ def clean_text(raw_text):
     return simplied_text
 
 def seperate_langs(original_text):
+    result = {'chn_text': '', 'eng_text': ''}
     chn_text = ''.join(regex.findall(CHN_REGEX, original_text))
     eng_text = regex.sub(CHN_REGEX, '', original_text)
+    chn_text = demoji.replace(chn_text)
+    eng_text = demoji.replace(eng_text)
 
-    return [chn_text, eng_text]
+    if is_valid_monolingual_str(chn_text, 'chn'):
+        result['chn_text'] = chn_text
+    if is_valid_monolingual_str(eng_text, 'eng'):
+        result['eng_text'] = eng_text
+
+    return result
+
+def is_valid_monolingual_str(text, lang):
+    if regex.match(MEANINGLESS_REGEX, text):
+        return False
+    elif lang == 'chn' and len(text) < 5:
+        return False
+    elif lang == 'eng' and len(text.split()) < 3:
+        return False
+    else:
+        return True
 
 df = pd.read_csv('../data/tweets_original.csv')
+
+print('Read successful!, shape={}'.format(df.shape))
 
 df['clean_text'] = df['raw_text'].apply(clean_text)
 df = df.drop(df[df['clean_text'].str.len() <= 0].index)
 
-df['monolingual_text'] = df['clean_text'].apply(seperate_langs)
-df = df.explode('monolingual_text')
+monolingual_texts = df['clean_text'].apply(seperate_langs)
+df = pd.concat([df, monolingual_texts.apply(pd.Series)], axis=1)
 df = df.reset_index(drop=True)
-df = df.drop(df[df['monolingual_text'].str.len() < 5].index)
+df = df.drop(df[(df['chn_text'].str.len() <= 0) & (df['eng_text'].str.len() <= 0)].index)
+df = df.drop('clean_text', axis=1)
+df = df.reset_index(drop=True)
 
 low_freq_users = df['screen_name'].value_counts()
 to_remove = low_freq_users[low_freq_users < 30].index
 df = df.drop(to_remove)
-
 df = df.reset_index(drop=True)
+
+print('Clean successful!, shape={}'.format(df.shape))
 
 df.to_csv('../data/tweets.csv', quoting=csv.QUOTE_NONNUMERIC)
---
+++ code/translation.py
@@ -0,0 +1,50 @@
+import os, requests, uuid, json, csv
+import pandas as pd
+
+def _get_azure_translations(text, src, dest):
+    key_var_name = 'TRANSLATOR_TEXT_SUBSCRIPTION_KEY'
+    if not key_var_name in os.environ:
+        raise Exception('Please set/export the environment variable: {}'.format(key_var_name))
+    subscription_key = os.environ[key_var_name]
+
+    endpoint_var_name = 'TRANSLATOR_TEXT_ENDPOINT'
+    if not endpoint_var_name in os.environ:
+        raise Exception('Please set/export the environment variable: {}'.format(endpoint_var_name))
+    endpoint = os.environ[endpoint_var_name]
+
+    # If you encounter any issues with the base_url or path, make sure
+    # that you are using the latest endpoint: https://docs.microsoft.com/azure/cognitive-services/translator/reference/v3-0-translate
+    path = '/translate?api-version=3.0'
+    params = '&from={}&to={}'.format(src, dest)
+    constructed_url = endpoint + path + params
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': subscription_key,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    # You can pass more than one object in body.
+    body = [{
+        'text' : text
+    }]
+    request = requests.post(constructed_url, headers=headers, json=body)
+    response = request.json()
+
+    return response[0]['translations'][0]['text']
+
+def translate(text, src='zh-tw', dest='en'):
+    print('Translating dest=', dest, 'text=', text)
+    return _get_azure_translations(text, src=src, dest=dest)
+
+df = pd.read_csv('../data/tweets.csv')
+
+df['translated_eng'] = df['chn_text'].dropna().apply(translate)
+print('-----------')
+print('Translate ENG successful!, shape={}'.format(df.shape))
+
+df['translated_chn'] = df['eng_text'].dropna().apply(translate, src='en', dest='zh-tw')
+print('-----------')
+print('Translate CHN successful!, shape={}'.format(df.shape))
+
+df.to_csv('../data/tweets_translated.csv', quoting=csv.QUOTE_NONNUMERIC)
